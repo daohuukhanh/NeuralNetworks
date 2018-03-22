b45772732454c62215b322eb6c80b2a8465383c2
ianianda <ywang402@my.bcit.ca>
2018-03-22 16:09:31 -0700

    implementing network.cs

--- WindowsFormsApp1/WindowsFormsApp1/network.cs
+++ WindowsFormsApp1/WindowsFormsApp1/network.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System; // include Random library
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections; // include array
+using System.Collections.Generic;
+
+/* https://en.wikipedia.org/wiki/NumPy */
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +18,25 @@ namespace WindowsFormsApp1
      *  **/
     class Network
     {
+        private int num_layers;
+        private List<int> sizes; // contains the number of neurons in the respective layers of the network.
+        private int biases; // randomly initialized
+        private int weights; // randomly initialized
 
+        public Network(int num, List<int> size)
+        {
+            this.num_layers = num;
+            this.sizes = size;
+            //this.biases = bias;
+            //this.weights = wh;
+        }
+
+        public int GetRandomNum(int n) // implementing Gauss...
+        {
+            //private int num;
+
+
+            //return num;
+        }
     }
 }
